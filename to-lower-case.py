f0ae6bcb4c7611a7bac3b893c9374ba94edd764a
Andreea <28634684+olaruandreea@users.noreply.github.com>
2019-12-26 21:52:33 +0000

    Add files via upload

---
+++ to-lower-case.py
@@ -0,0 +1,15 @@
+'''
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+'''
+
+class Solution(object):
+    def toLowerCase(self, str):
+        return str.lower()
+        
