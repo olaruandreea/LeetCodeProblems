9fdca80e03121b79e4793abbd69525e06d113ef6
Andreea <28634684+olaruandreea@users.noreply.github.com>
2020-01-11 21:23:43 +0000

    Add files via upload

--- remove-element.py
+++ remove-element.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        return [x for x in nums if int(x) != int(val)]
+        nums[:] = [x for x in nums if int(x) != int(val)]
+        return len(nums)
 
-nums =  [3,2,2,3]
-val = 3
+nums =  [1,4,3,3,3,4]
+val = 4
 Test = Solution().removeElement(nums,val)
 print Test
