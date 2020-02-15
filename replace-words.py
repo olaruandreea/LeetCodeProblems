17683abcb5ce5e1d05406999a35b425726b13cbd
Andreea <28634684+olaruandreea@users.noreply.github.com>
2020-02-15 18:15:32 +0000

    Add files via upload

---
+++ replace-words.py
@@ -0,0 +1,36 @@
+"""
+In English, we have a concept called root, which can be followed by some other words to form another longer word - let's call this word successor. For example, the root an, followed by other, which can form another word another.
+
+Now, given a dictionary consisting of many roots and a sentence. You need to replace all the successor in the sentence with the root forming it. If a successor has many roots can form it, replace it with the root with the shortest length.
+
+You need to output the sentence after the replacement.
+
+Example 1:
+
+Input: dict = ["cat", "bat", "rat"]
+sentence = "the cattle was rattled by the battery"
+Output: "the cat was rat by the bat"
+"""
+
+class Solution(object):
+	def replaceWords(self, dict, sentence):
+		tokens = sentence.split(" ")
+		i = 0
+		s = ""
+		while i < len(tokens):
+			x = sorted([word for word in dict if tokens[i].startswith(word)], key=len)
+			if len(x) == 0:
+				s += tokens[i] + " "
+			elif len(x) == 1:
+				s += ' '.join(map(str, x)) + " " 
+			else:
+				s += ''.join(map(str, x[0])) + " " 
+			i+=1		
+		return len(s[0:-1])
+
+
+
+dict  = ["cat", "bat", "catt","rat"]
+sentence = "the cattle was rattled by the battery"
+print Solution().replaceWords(dict,sentence)
+
